6e12ca77b177e6306a6e7e6c1a5a6715bcc0bab8
Konstantin S. <havendv@gmail.com>
2026-05-05 20:30:22 +0400

    feat: Updated OpenAPI spec (#5)
    Co-authored-by: github-actions[bot] <dependabot@bot.com>

--- src/libs/PlayHT/Generated/PlayHT.IWebSocketClient.CreateWebSocketAuth.g.cs
+++ src/libs/PlayHT/Generated/PlayHT.IWebSocketClient.CreateWebSocketAuth.g.cs
@@ -13,5 +13,14 @@ namespace PlayHT
         global::System.Threading.Tasks.Task<global::PlayHT.WebSocketAuthResponse> CreateWebSocketAuthAsync(
             global::PlayHT.AutoSDKRequestOptions? requestOptions = default,
             global::System.Threading.CancellationToken cancellationToken = default);
+        /// <summary>
+        /// Create short-lived WebSocket URLs for realtime TTS
+        /// </summary>
+        /// <param name="requestOptions">Per-request overrides such as headers, query parameters, timeout, retries, and response buffering.</param>
+        /// <param name="cancellationToken">The token to cancel the operation with</param>
+        /// <exception cref="global::PlayHT.ApiException"></exception>
+        global::System.Threading.Tasks.Task<global::PlayHT.AutoSDKHttpResponse<global::PlayHT.WebSocketAuthResponse>> CreateWebSocketAuthAsResponseAsync(
+            global::PlayHT.AutoSDKRequestOptions? requestOptions = default,
+            global::System.Threading.CancellationToken cancellationToken = default);
     }
 }
